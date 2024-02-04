c814a89050b01676278f2ccab83ea0c449769417
Rafid41 <wap.alnahiyan525@gmail.com>
2024-02-04 17:08:25 +0600

    Styling navigation Header

--- app/AppNavigator.js
+++ app/AppNavigator.js
@@ -13,9 +13,24 @@ const Stack = createStackNavigator();
 
 const MenuStack = () => {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#F53B50",
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                    fontWeight: "bold",
+                },
+            }}
+        >
             <Stack.Screen name="Menu" component={MenuScreen} />
-            <Stack.Screen name="Dish Detail" component={DishDetailScreen} />
+            <Stack.Screen
+                name="Dish Detail"
+                component={DishDetailScreen}
+                // dish_name er name show korbe nav title e
+                options={({ route }) => ({ title: route.params.dish.name })}
+            />
         </Stack.Navigator>
     );
 };
