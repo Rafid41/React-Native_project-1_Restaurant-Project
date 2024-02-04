ad50eb58454eb966f6b8f64058e96625850809ad
Rafid41 <wap.alnahiyan525@gmail.com>
2024-02-04 07:27:15 +0600

    create app/components and app/screens structure

---
+++ app/AppNavigator.js
@@ -0,0 +1,14 @@
+// app\AppNavigator.js
+// controls all navigation fuctionalities
+import React from "react";
+import { View, Text } from "react-native";
+
+const AppNavigator = () => {
+    return (
+        <View>
+            <Text>AppNavigator</Text>
+        </View>
+    );
+};
+
+export default AppNavigator;
---
+++ app/screens/DishDetailScreen.js
@@ -0,0 +1,13 @@
+// app\screens\DishDetailScreen.js
+import React from "react";
+import { View, Text } from "react-native";
+
+const DishDetailScreen = () => {
+    return (
+        <View>
+            <Text>DishDetail Screen</Text>
+        </View>
+    );
+};
+
+export default DishDetailScreen;
